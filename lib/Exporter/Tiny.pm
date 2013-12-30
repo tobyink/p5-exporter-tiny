63a9fd8533637fae34d412b0c9a7159ff30422da
Toby Inkster <mail@tobyinkster.co.uk>
2013-12-30 10:23:24 +0000

    bump version; changelog

--- lib/Exporter/Tiny.pm
+++ lib/Exporter/Tiny.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings; no warnings qw(void once uninitialized numeric redefine);
 
 our $AUTHORITY = 'cpan:TOBYINK';
-our $VERSION   = '0.030';
+our $VERSION   = '0.031_01';
 our @EXPORT_OK = qw< mkopt mkopt_hash _croak >;
 
 sub _croak ($;@) { require Carp; my $fmt = shift; @_ = sprintf($fmt, @_); goto \&Carp::croak }
