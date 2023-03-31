37e022e2a220552cb5f342c2addfa7401356320e
Toby Inkster <mail@tobyinkster.co.uk>
2023-03-31 13:29:36 +0100

    Link to Exporter::Almighty

--- lib/Exporter/Tiny.pm
+++ lib/Exporter/Tiny.pm
@@ -537,6 +537,8 @@ on L<irc.perl.org|http://www.irc.perl.org/channels.html>.
 
 Simplified interface to this module: L<Exporter::Shiny>.
 
+Less tiny version, with more features: L<Exporter::Almighty>.
+
 Other interesting exporters: L<Sub::Exporter>, L<Exporter>.
 
 =head1 AUTHOR
@@ -545,7 +547,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2013-2014, 2017, 2022 by Toby Inkster.
+This software is copyright (c) 2013-2014, 2017, 2022-2023 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
